1bb19870fc69142c3d8df3cf245746850ba38ae4
Maltmana <Maltmana@users.noreply.github.com>
2018-09-19 14:04:04 -0700

    facing direction

--- Raylib/Creature.h
+++ Raylib/Creature.h
@@ -23,17 +23,54 @@ public:
 	// member functions
 	void Update(Vector2 p_targPos)
 	{
-		m_pos = Vector2Subtract(m_pos, Vector2Normalize(Vector2Subtract(m_pos,p_targPos)));
+		Vector2 subtractedVector = Vector2Subtract(p_targPos, m_pos);
+		printf("%f\n", subtractedVector.y);
+		m_pos = Vector2Add(m_pos, Vector2Normalize(subtractedVector));
+		if (subtractedVector.y < 0 && (subtractedVector.x < 100 && subtractedVector.x > -100))
+		{
+			m_facing = 0;
+			return;
+		}
+		if (subtractedVector.y >= 0 && (subtractedVector.x < 100 && subtractedVector.x > -100))
+		{
+			m_facing = 2;
+			return;
+		}
+		if (subtractedVector.x < 0)
+		{
+			m_facing = 3;
+		}
+		if (subtractedVector.x >= 0)
+		{
+			m_facing = 1;
+		}
+
+
 	}
 	void UpdateAnimation()
 	{
+		// connecting the facing to the animation row here.
+		switch (m_facing)
+		{
+			case 0: currentFrameRow = 0;
+				break;
+			case 1: currentFrameRow = 1;
+				break;
+			case 2: currentFrameRow = 2;
+				break;
+			case 3: currentFrameRow = 3;
+				break;
+		default:
+			break;
+		}
+
+		// animation column.
 		elapsedAnimTime = elapsedAnimTime + GetFrameTime();
 		if (elapsedAnimTime >= animTime)
 		{
 			if (rising)
 			{
 				currentFrame += 1;
-				printf("%d\n", currentFrame);
 				if (currentFrame == frameAmount)
 				{
 					rising = false;
@@ -42,7 +79,6 @@ public:
 			else
 			{
 				currentFrame -= 1;
-				printf("%d\n", currentFrame);
 				if (currentFrame == 1)
 				{
 					rising = true;
@@ -80,9 +116,12 @@ public:
 
 	// member variables
 	Vector2 m_pos;
+	Vector2 m_targPos;
+	int m_facing = 0;
 	float animTime = 1.f;
 	float elapsedAnimTime = 0.f;
 	bool rising = true;
 	int currentFrame = 2;
+	int currentFrameRow = 0;
 	int frameAmount = 3;
 };
