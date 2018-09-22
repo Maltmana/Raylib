ccb2860cba049ffb0713d32128e6c1c85a1bf399
Maltmana <Maltmana@users.noreply.github.com>
2018-09-21 17:47:52 -0700

    got a grass tile

--- Raylib/Graphic.h
+++ Raylib/Graphic.h
@@ -3,13 +3,29 @@
 class Graphic
 {
 public:
-	Graphic(std::string p_path)
+	Graphic(const char * p_path, int p_nSpritesCol, int p_nSpritesRow)
 		:
-		texture{ LoadTexture(p_path.c_str()) }
+		m_texture{ LoadTexture(p_path) },
+		m_nSpritesCol{ p_nSpritesCol },
+		m_nSpriteRow{ p_nSpritesRow }
 	{
+	}
+	~Graphic()
+	{
+		UnloadTexture(m_texture);
 	}
 	// member funcs
 
 	// member vars
-	Texture2D const texture;
+	Texture2D const m_texture;
+	int const m_nSpritesCol;
+	int const m_nSpriteRow;
+	int const m_frameWidth = m_texture.width / m_nSpritesCol;
+	int const m_frameHeight = m_texture.height / m_nSpriteRow;
+	// NOTE: Source rectangle (part of the texture to use for drawing)
+	Rectangle m_sourceRec = { 0, 0, (float)m_frameWidth, (float)m_frameHeight };
+	// NOTE: Destination rectangle (screen rectangle where drawing part of texture)
+	Rectangle destRec = { 0,0, (float)m_frameWidth * 2, (float)m_frameHeight * 2 };
+	// NOTE: Origin of the texture (rotation/scale point), it's relative to destination rectangle size
+	Vector2 origin = { (float)m_frameWidth, (float)m_frameHeight };
 };
